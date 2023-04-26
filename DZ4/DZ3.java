cde2fed4bf1c4d92f92e508595e73a228e2d732a
anton.chuchalin <anton.chuchalin@mail.ru>
2023-04-26 18:23:10 +0500

    add  DZ3

---
+++ DZ4/DZ3.java
@@ -0,0 +1,24 @@
+package DZ4;
+
+import java.util.LinkedList;
+import java.util.ListIterator;
+
+public class DZ3 {
+    public static void main(String[] args) {
+        LinkedList<Integer> list = DZ1.createList(3, 0, 50);
+        System.out.println(list);
+        System.out.println(sumList(list));
+    }
+
+    static int sumList(LinkedList<Integer> list){
+        int sum = 0;
+//        for (int x: list) {
+//            sum += x;
+//        }
+        ListIterator<Integer> iterator = list.listIterator();
+        while(iterator.hasNext()){
+            sum += iterator.next();
+        }
+        return sum;
+    }
+}
